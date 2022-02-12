a4c857578c915cf3eac4cc1bc0115930a014d832
LevanSamadashvili <lsama19@freeuni.edu.ge>
2022-02-12 22:11:15 +0400

    started testing

--- App/tests/test_handlers.py
+++ App/tests/test_handlers.py
@@ -1,12 +1,18 @@
+import unittest
 from dataclasses import dataclass
 
+from App.core import status
 from App.core.bitcoin_core import default_api_key_generator
 from App.core.constants import MAX_AVAILABLE_WALLETS
-from App.core.core_responses import RegisterUserResponse, CoreResponse
-from App.core.handlers import CreateUserHandler, NoHandler, IHandle, HasUserHandler, MaxWalletsHandler
+from App.core.core_responses import CoreResponse, RegisterUserResponse
+from App.core.handlers import (
+    CreateUserHandler,
+    HasUserHandler,
+    IHandle,
+    MaxWalletsHandler,
+    NoHandler,
+)
 from App.infra.repositories.user_repository import InMemoryUserRepository
-
-from App.core import status
 from App.infra.repositories.wallet_repository import InMemoryWalletRepository
 
 
@@ -19,79 +25,82 @@ class HandlerForTest(IHandle):
         return CoreResponse(status_code=0)
 
 
-def test_no_handler() -> None:
-    handler = NoHandler()
-    response = handler.handle()
+class TestHandlers(unittest.TestCase):
+    def setUp(self) -> None:
+        self.user_repository = InMemoryUserRepository()
+        self.wallet_repository = InMemoryWalletRepository()
 
-    assert response.status_code == 0
+        self.test_handler = HandlerForTest()
 
-def test_should_create_user() -> None:
-    user_repo = InMemoryUserRepository()
-    handler = CreateUserHandler(next_handler=NoHandler(),
-                                user_repository=user_repo,
-                                api_key_generator_strategy=default_api_key_generator)
+    def test_no_handler(self) -> None:
+        handler = NoHandler()
+        response = handler.handle()
 
-    response = handler.handle()
-    assert response.status_code == status.USER_CREATED_SUCCESSFULLY
-    assert isinstance(response, RegisterUserResponse)
-    assert user_repo.has_user(api_key=response.api_key)
+        assert response.status_code == 0
 
+    def test_should_create_user(self) -> None:
+        handler = CreateUserHandler(
+            next_handler=NoHandler(),
+            user_repository=self.user_repository,
+            api_key_generator_strategy=default_api_key_generator,
+        )
 
-def test_should_not_have_user() -> None:
-    user_repo = InMemoryUserRepository()
+        response = handler.handle()
+        assert response.status_code == status.USER_CREATED_SUCCESSFULLY
+        assert isinstance(response, RegisterUserResponse)
+        assert self.user_repository.has_user(api_key=response.api_key)
 
-    handler = HasUserHandler(next_handler=NoHandler(),
-                             api_key="trash",
-                             user_repository=user_repo)
+    def test_should_not_have_user(self) -> None:
+        handler = HasUserHandler(
+            next_handler=NoHandler(),
+            api_key="trash",
+            user_repository=self.user_repository,
+        )
 
-    response = handler.handle()
-    assert response.status_code == status.INCORRECT_API_KEY
+        response = handler.handle()
+        assert response.status_code == status.INCORRECT_API_KEY
 
+    def test_should_have_user(self) -> None:
+        api_key = "test_api_key"
+        self.user_repository.create_user(api_key=api_key)
 
-def test_should_have_user() -> None:
-    api_key = "test_api_key"
-    user_repo = InMemoryUserRepository()
-    user_repo.create_user(api_key=api_key)
+        handler = HasUserHandler(
+            next_handler=self.test_handler,
+            api_key=api_key,
+            user_repository=self.user_repository,
+        )
 
-    next_handler = HandlerForTest()
+        handler.handle()
+        assert self.test_handler.was_called
 
-    handler = HasUserHandler(next_handler=next_handler,
-                             api_key=api_key,
-                             user_repository=user_repo)
+    def test_can_create_another_wallet(self) -> None:
+        api_key = "test_api_key"
+        self.user_repository.create_user(api_key=api_key)
 
-    handler.handle()
-    assert next_handler.was_called
+        handler = MaxWalletsHandler(
+            next_handler=self.test_handler,
+            api_key=api_key,
+            wallet_repository=self.wallet_repository,
+        )
 
+        handler.handle()
+        assert self.test_handler.was_called
 
-def test_can_create_another_wallet() -> None:
-    user_repo = InMemoryUserRepository()
-    wallet_repo = InMemoryWalletRepository()
+    def test_cant_create_more_wallets(self) -> None:
+        api_key = "test_api_key"
+        self.user_repository.create_user(api_key=api_key)
 
-    api_key = "test_api_key"
-    user_repo.create_user(api_key=api_key)
+        for i in range(MAX_AVAILABLE_WALLETS):
+            self.wallet_repository.create_wallet(address=f"wallet{i}", api_key=api_key)
 
-    next_handler = HandlerForTest()
-    handler = MaxWalletsHandler(next_handler=next_handler,
-                                api_key=api_key,
-                                wallet_repository=wallet_repo)
+        handler = MaxWalletsHandler(
+            next_handler=NoHandler(),
+            api_key=api_key,
+            wallet_repository=self.wallet_repository,
+        )
 
-    handler.handle()
-    assert next_handler.was_called
+        response = handler.handle()
+        assert response.status_code == status.CANT_CREATE_MORE_WALLETS
 
-def test_cant_create_more_wallets() -> None:
-    user_repo = InMemoryUserRepository()
-    wallet_repo = InMemoryWalletRepository()
-
-    api_key = "test_api_key"
-    user_repo.create_user(api_key=api_key)
-
-    for i in range(MAX_AVAILABLE_WALLETS):
-        wallet_repo.create_wallet(address=f"wallet{i}", api_key=api_key)
-
-    handler = MaxWalletsHandler(next_handler=NoHandler(),
-                                api_key=api_key,
-                                wallet_repository=wallet_repo)
-
-    response = handler.handle()
-    assert response.status_code == status.CANT_CREATE_MORE_WALLETS
-
+    def test_should_create_wallet(self) -> None:
+        pass
