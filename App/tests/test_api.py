3f2d8e2608be6a1de489138e03ff4a597391e691
LevanSamadashvili <lsama19@freeuni.edu.ge>
2022-02-13 14:26:38 +0400

    mypy green

---
+++ App/tests/test_api.py
@@ -0,0 +1,32 @@
+import unittest
+
+from starlette.responses import Response
+
+from App.core.bitcoin_core import BitcoinCore, default_api_key_generator, default_address_generator, \
+    default_transaction_fee
+from App.core.btc_usd import default_btc_usd_convertor
+from App.infra.repositories.statistics_repository import InMemoryStatisticsRepository
+from App.infra.repositories.transactions_repository import InMemoryTransactionsRepository
+from App.infra.repositories.user_repository import InMemoryUserRepository
+from App.infra.repositories.wallet_repository import InMemoryWalletRepository
+from App.runner.api import register_user
+
+
+def get_in_memory_core() -> BitcoinCore:
+    return BitcoinCore(user_repository=InMemoryUserRepository(),
+                       wallet_repository=InMemoryWalletRepository(),
+                       transactions_repository=InMemoryTransactionsRepository(),
+                       statistics_repository=InMemoryStatisticsRepository(),
+                       api_key_generator_strategy=default_api_key_generator,
+                       address_generator_strategy=default_address_generator,
+                       btc_usd_convertor_strategy=default_btc_usd_convertor,
+                       transaction_fee_strategy=default_transaction_fee)
+
+
+class TestApi(unittest.TestCase):
+    def setUp(self) -> None:
+        self.in_memory_core = get_in_memory_core()
+
+    def test_register_user(self) -> None:
+        response = register_user(response=Response(), bitcoin_core=self.in_memory_core)
+        print(response)
